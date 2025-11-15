6b5be7f9d919b6f60bf8c101a579a002d4f16c0c
abomjan <adarshabomjan11@gmail.com>
2025-11-14 18:02:28 -0800

    Add hover effects for buttons

--- Assignments/4/scripts/calculator.js
+++ Assignments/4/scripts/calculator.js
@@ -1,53 +1,47 @@
-const display = document.getElementById('display');
+const display = document.getElementById("display");
 
-const buttons = Array.from(document.querySelectorAll('button'));
+const buttons = Array.from(document.querySelectorAll("button"));
 
-buttons.forEach(buttons=>{
-    buttons.addEventListener('click', (e)=>{
-        const buttonText = e.target.innerText;
+buttons.forEach((buttons) => {
+  buttons.addEventListener("click", (e) => {
+    const buttonText = e.target.innerText;
 
-        if(buttonText === 'AC'){
-            clearDisplay();
-        }
-        else if(buttonText === '='){
-            calculate();
-        }
-        else if(buttonText === '+/-'){
-            toggleSign();
-        }
-        else if(buttonText === 'X'){
-            appendToDisplay('*');
-        }
-        else{
-            appendToDisplay(buttonText);
-        }
-    });
+    if (buttonText === "AC") {
+      clearDisplay();
+    } else if (buttonText === "=") {
+      calculate();
+    } else if (buttonText === "+/-") {
+      toggleSign();
+    } else if (buttonText === "X") {
+      appendToDisplay("*");
+    } else {
+      appendToDisplay(buttonText);
+    }
+  });
 });
 
 const appendToDisplay = (input) => {
-    display.value += input;
+  display.value += input;
 };
 
 const clearDisplay = () => {
-    display.value = "";
+  display.value = "";
 };
 
 const toggleSign = () => {
-    if(display.value){
-        if(display.value.startsWith('-')){
-            display.value = display.value.slice(1);
-        }
-        else{
-            display.value = '-' + display.value;
-        }
+  if (display.value) {
+    if (display.value.startsWith("-")) {
+      display.value = display.value.slice(1);
+    } else {
+      display.value = "-" + display.value;
     }
+  }
 };
 
 const calculate = () => {
-    try{
-        display.value = eval(display.value);
-    }
-    catch(error){
-        display.value = "Error";
-    }
+  try {
+    display.value = eval(display.value);
+  } catch (error) {
+    display.value = "Error";
+  }
 };
