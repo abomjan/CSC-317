b3a239bc2a00b8a137e918bcbac2451f21b82d7f
abomjan <adarshabomjan11@gmail.com>
2025-11-14 17:32:23 -0800

    Add function to display the output using try and catch

--- Assignments/4/scripts/calculator.js
+++ Assignments/4/scripts/calculator.js
@@ -25,3 +25,12 @@ const appendToDisplay = (input) => {
 const clearDisplay = () => {
     display.value = "";
 };
+
+const calculate = () => {
+    try{
+        display.value = eval(display.value);
+    }
+    catch(error){
+        display.value = "Error";
+    }
+};
