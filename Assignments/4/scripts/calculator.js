b4f3348934d1f0186767d94ee4c78637e740989a
abomjan <adarshabomjan11@gmail.com>
2025-11-15 15:00:03 -0800

    Add keyboard event listener

--- Assignments/4/scripts/calculator.js
+++ Assignments/4/scripts/calculator.js
@@ -45,3 +45,49 @@ const calculate = () => {
     display.value = "Error";
   }
 };
+
+document.addEventListener("keydown", function (event) {
+  const key = event.key;
+
+  if (key >= "0" && key <= "9") {
+    appendToDisplay(key);
+  }
+
+  switch (key) {
+    case "+":
+      appendToDisplay("+");
+      break;
+    case "-":
+      appendToDisplay("-");
+      break;
+    case "*":
+    case "x":
+    case "X":
+      appendToDisplay("*");
+      break;
+    case "/":
+      appendToDisplay("/");
+      event.preventDefault();
+      break;
+    case "%":
+      appendToDisplay("%");
+      break;
+    case ".":
+      appendToDisplay(".");
+      break;
+    case "Enter":
+    case "=":
+      calculate();
+      event.preventDefault();
+      break;
+    case "Escape":
+    case "c":
+    case "C":
+      clearDisplay();
+      break;
+    case "Backspace":
+      display.value = display.value.slice(0, -1);
+      event.preventDefault();
+      break;
+  }
+});
